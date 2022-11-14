460d147daea6300e521308e586f9c0c5203cb973
Tsukuyomi-808 <froilanxx3@gmail.com>
2022-11-14 12:25:01 -1000

    commit

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -3,71 +3,66 @@ from flask_app import app
 from flask_app.models.user_model import User
 from flask_bcrypt import Bcrypt
 
-bcrypt = Bcrypt ( app )
+bcrypt = Bcrypt(app)
 
-@app.route( "/" )
+@app.route("/")
 def display_login_registration():
-  return render_template( "index.html" )
+    return render_template("index.html")
 
 
-# This is validating the email exist
-# If it doesnt exit then you create a new one
-@app.route( "/user/new", methods = ['POST'] )
+@app.route("/user/new", methods = ['POST'])
 def create_user():
-  
-  if User.validate_register == True:
-  
-    data = {
-      "email" : request.form[ 'email' ]
-    }
-    result = User.get_one( data )
-    if result == None:
-      # Add the new user
-      data ={
-        "email" : request.form['email'],
-        "first_name" :request.form['first_name'],
-        "last_name" :request.form['last_name'],
-        "password" : bcrypt.generate_password_hash( request.form[ 'password'] )
-      }
-      user_id = User.create( data )
-      
-      session[ 'email' ] = request.form[ 'email' ],
-      session[ 'first_name' ] = request.form[ 'first_name' ],
-      session[ 'last_name' ] = request.form[ 'last_name' ]
-      session[ 'user_id' ] = user_id
-      return redirect ( "/dashboard" )
+    if User.validate_register(request.form) == True:
+        data = {
+            "email" : request.form['email']
+        }
+
+        result = User.get_one(data)
+        if result == None:
+            # Add the new user
+            data = {
+                "email" : request.form['email'],
+                "first_name" : request.form['first_name'],
+                "last_name" : request.form['last_name'],
+                "password" : bcrypt.generate_password_hash(request.form['password'])
+            }
+            user_id = User.create(data)
+            session['email'] = request.form['email']
+            session['first_name'] = request.form['first_name']
+            session['last_name'] = request.form['last_name']
+            session['user_id'] = user_id
+            return redirect("/dashboard")
+        else:
+            flash("Email already in use, please provide another", "error_register_email")
+            return redirect("/")
     else:
-      flash( "That email already exists, please select another one.", "error_register_email" )
-      pass
-  else:
-    return redirect ( "/" )
+        return redirect("/")
 
 
-
-@app.route( "/logout" )
+@app.route("/logout")
 def logout():
-  session.clear()
-  return redirect ("/")
+    session.clear()
+    return redirect("/")
 
 
-
-@app.route( "/login", methods = [ 'POST' ] )
+@app.route("/login", methods = ['POST'])
 def login():
-  data = {
-    "email" : request.form [ 'email' ] #Making sure to validate the email
-  }
-  result = User.get_one( data ) 
-  
-  if result == None:
-    flash( "Wrong credentials.", "error_login")
-    return redirect ("/") #if wrong conditions back to index.html
-  else:
-    if not bcrypt.check_password_hash( result.password, request.form[ 'password'] ):
-      flash( "wrong password", "error_login")
-      return redirect ("/") #if wrong conditions back to index.html
+    data = {
+        "email" : request.form['email']
+    }
+
+    result = User.get_one(data)
+
+    if result == None:
+        flash("Wrong Cridentials", "error_login")
+        return redirect("/")
     else:
-      session[ 'email' ] = result.email
-      session[ 'first_name' ] = result.first_name
-      session[ 'last_name' ] = result.last_name
-      session[ 'user_id' ] = result.id
-      return redirect( "/dashboard" ) #If all conditions meet it will go to dashboard
+        if not bcrypt.check_password_hash(result.password, request.form['password']):
+            flash("Wrong Cridentials", "error_login")
+            return redirect("/")
+        else:
+            session['email'] = result.email
+            session['first_name'] = result.first_name
+            session['last_name'] = result.last_name
+            session['user_id'] = result.id
+            return redirect("/dashboard")
